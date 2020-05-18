c12533ad859d9734ae5c2159fccb52bf5bc91ac0
Robert Buck <rbuck@nuodb.com>
2020-05-18 09:13:27 -0400

    fix: Remove chmod as this will not work OOTB with NFS mounts due to root squash.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,15 +6,6 @@ if [ "${1:0:1}" = '-' ]; then
     set -- fio "$@"
 fi
 
-if [ "$1" = 'fio' -o "$1" = 'ioping' ]; then
-  if [ "$(stat -c %u /iops/config)" != "$(id -u iops)" ]; then
-    chown iops:iops /iops/config
-  fi
-  if [ "$(stat -c %u /iops/data)" != "$(id -u iops)" ]; then
-    chown iops:iops /iops/data
-  fi
-fi
-
 if [ "$1" = 'fio' ]; then
   set -- su-exec iops:iops "$@"
 elif [ "$1" = 'ioping' ]; then
